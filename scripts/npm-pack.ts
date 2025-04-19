98bfb435e6d7f9bd19a4f2e71557df3050d3c471
Rodney, Tiara <tiara.rodney@administratrix.de>
2025-04-19 04:55:26 +0200

    refactor(scripts): remove dependencies
    No longer dependent upon yargs and glob

--- scripts/npm-pack.ts
+++ scripts/npm-pack.ts
@@ -1,12 +1,19 @@
-import * as yargs from 'yargs';
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import * as child_process from 'child_process';
+import * as child_process from 'node:child_process';
+import * as fs from 'node:fs';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import * as util from 'node:util';
 
-import { globIterateSync } from 'glob';
+const DEFAULT_INPUT_DIR: string = path.join('build', 'release');
+const DEFAULT_OUTPUT_DIR: string = 'dist';
+const DEFAULT_ASSETS_INDEX_BASENAME: string = 'assets.txt';
+const DEFAULT_DOCS_DIRNAME: string = '_docs';
 
-const usage = `Create a tarball from a package
+function usage(exec: string): string {
+    return `
+Usage: ${exec} [OPTIONS] [INPUT] [OUTPUT] [DOCS]
+
+Create a tarball from a package
 
 This script is a wrapper around \`npm pack\`. 
 
@@ -29,141 +36,274 @@ isn't compatible with npm pack routine. With npm packages, usually build
 artifacts are transpiled into the same directory as their sources and then
 excluded via e.g.  \`.gitignore\`. This is how the \`npm pack\` routine expects
 it, which gives poor isolation between sources and build artifacts.
-`
 
-const argv = require('yargs')
-    // yargs doesn't adhere to established standards and reinvents the wheel
-    // with this convention of defining the usage... Really annoying, as it does
-    // not allow for indicating options, since it will be parsed as a positional
-    // argument... Yes, I'm starting to doubt my choice of yargs and considering
-    // migrating to commander.js. Oy! 200+ issues for an argument parser...
-    .usage('$0 inputDir outputDir [docsDir]', usage)
-    .positional('inputDir', {
-            type: 'string',
-            default: 'build/release',
-            describe: 'path to package build'
-    })
-    .positional('outputDir', {
-            type: 'string',
-            default: 'dist',
-            describe: 'path to dist directory'
-    })
-    .positional('docsDir', {
-            type: 'string',
-            default: null,
-            describe: 'path to docs build'
-    })
-    .demandOption(['inputDir', 'outputDir'])
-    .help()
-    .argv;
+Positional arguments:
 
-var cwd = process.cwd();
+    INPUT  - directory of build output used as the input for packaging
+             [default:${DEFAULT_INPUT_DIR}]
 
-if (path.dirname(argv.inputDir) == path.basename(argv.inputDir)) {
-    throw new Error('inputDir must have a nesting depth of at least 2')
+    OUTPUT - directory to output the package (tarball) into
+             [default:${DEFAULT_OUTPUT_DIR}]
+
+    DOCS   - directory containing documentation, which is used as an auxiliary
+             input for packaging alongside INPUT.
+
+Options:
+
+    -a, --assets-index-basename - basename of assets index. This is the output 
+                                  file that contains a listing of all the output
+                                  files which are meant to be used by the system
+                                  integrating the HTML theme.
+                                  [default:${DEFAULT_ASSETS_INDEX_BASENAME}]
+
+    -d, --docs-dirname          - name of directory to output documentation into
+                                  (if supplied through DOCS).
+                                  [default:${DEFAULT_DOCS_DIRNAME}]
+`;
 }
 
-if ([path.sep, '.'].includes(argv.inputDir[0])) {
-    throw new Error(`inputDir must be a relative path inside of '${cwd}'`)
+export interface PackageOptions {
+    inputDir: string,
+    outputDir: string,
+    docsInputDir: string | null,
+    assetsIndexBasename: string,
+    docsOutputDirname: string,
 }
 
-const tempDir = fs.mkdtempSync(path.join(
-    os.tmpdir(),
-    `${path.basename(cwd)}-`
-));
+/**
+ * recursively traverse a directory and yield all paths relative to the root
+ *
+ * @param root - path where to start traversal from
+ */
+export function* listFiles(
+    root: string,
+    trueRoot?: string,
+): Generator<string, void, unknown> {
+    trueRoot = trueRoot ?? root;
+    if (!fs.existsSync(root)) {
+        throw new Error(`path does not exist: '${root}'`);
+    }
 
-fs.cpSync(
-    argv.inputDir,
-    tempDir,
-    {
-        recursive: true,
-        filter: (src: string, dest: string) => {
-            console.log(
-                `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
+    var files = fs.readdirSync(root);
+
+    for (var i = 0; i < files.length; i++) {
+        let filePath = path.join(root, files[i]);
+        let stat = fs.lstatSync(filePath);
+
+        if (stat.isDirectory()) { yield* listFiles(filePath, trueRoot) }
+        else { yield path.relative(trueRoot, filePath); }
+    };
+};
+
+/**
+ * strip a npm package specification
+ *
+ * @param specPath - path to specification (package.json)
+ */
+export function stripPackageSpec(specPath: string): object {
+    const packageSpec = JSON.parse(fs.readFileSync(
+        specPath,
+        { encoding: 'utf-8' }
+    ));
+
+    return {
+        name: packageSpec.name,
+        version: packageSpec.version,
+        description: packageSpec.description,
+        author: packageSpec.author,
+        private: true
+    };
+}
+
+/**
+ *
+ */
+function pack(options: PackageOptions): void {
+    var cwd = process.cwd();
+
+    if (path.dirname(options.inputDir) == path.basename(options.inputDir)) {
+        throw new Error('inputDir must have a nesting depth of at least 2')
+    }
+
+    if ([path.sep, '.'].includes(options.inputDir[0])) {
+        throw new Error(`inputDir must be a relative path inside of '${cwd}'`)
+    }
+
+    const tempDir = fs.mkdtempSync(path.join(
+        os.tmpdir(),
+        `${path.basename(cwd)}-`
+    ));
+
+    fs.cpSync(
+        options.inputDir,
+        tempDir,
+        {
+            recursive: true,
+            filter: (src: string, dest: string) => {
+                console.log(
+                    `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
+                );
+                return true;
+            }
+        }
+    );
+
+    [
+        'LICENSE',
+        'README.md',
+    ].forEach((target) => {
+        // TODO: sync file stats
+        fs.cpSync(
+            path.join(cwd, target),
+            path.join(tempDir, target),
+            {
+                filter: (src: string, dest: string) => {
+                    console.log(
+                        `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
+                    );
+                    return true;
+                }
+            }
+        );
+    });
+
+    if (options.docsInputDir) {
+        console.log('docs supplied, will copy...')
+        // TODO: sync file stats
+        fs.cpSync(
+            path.join(cwd, options.docsInputDir),
+            path.join(tempDir, options.docsOutputDirname),
+            {
+                recursive: true,
+                filter: (src: string, dest: string) => {
+                    console.log(
+                        `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
+                    );
+                    return true;
+                }
+            }
+        );
+    }
+
+    console.log(
+        `generating index (\'${options.assetsIndexBasename}\') of assets...`
+    );
+    // TODO: sync file stats
+    var fd: number = fs.openSync(
+        path.join(tempDir, options.assetsIndexBasename), 
+        'w'
+    );
+    for (const match of listFiles(options.inputDir)) {
+        fs.writeSync(fd, `${match}\n`);
+    }
+    fs.closeSync(fd);
+
+    console.log('generating npm package specification \'package.json\'...');
+    fs.writeFileSync(
+        path.join(tempDir, 'package.json'),
+        JSON.stringify(stripPackageSpec('package.json'), null, 4)
+    );
+
+    const outputDir = path.resolve(cwd, options.outputDir);
+    console.log(`mkdir: ${outputDir}`);
+    fs.mkdirSync(outputDir, { recursive: true });
+
+    console.log(`npm: pack --pack-destination ${outputDir}`);
+    child_process.execSync(
+        `npm pack --pack-destination ${outputDir}`,
+        {
+            cwd: tempDir,
+            stdio: "inherit"
+        }
+    );
+}
+
+if (require.main === module) {
+    // minimum number of positional arguments
+    const minPosargs: number = 2;
+
+    // default values of options arguments
+    const defaultOptargs: {[key: string]: any} = {
+        'assets-index-basename': DEFAULT_ASSETS_INDEX_BASENAME,
+        'docs-dirname': DEFAULT_DOCS_DIRNAME,
+    };
+
+    // required options arguments
+    const requiredOptargs: string[] = [
+        'docs-dirname',
+        'assets-index-basename',
+    ];
+
+    // the interface of parseArgs is very simple and Typescript does not play
+    // nicely with it, since it expects any reassignments to be of the same type
+    // as the primitives parseArgs allows. That's why I'm doing a lot of `as
+    // unknown as whatever` kung-fu down below. The node runtime doesn't care
+    // anyway...
+    var {values, positionals} = util.parseArgs({
+        options: {
+            'docs-dirname': {
+                type: 'string',
+                short: 'd'
+            },
+            'assets-index-basename': {
+                type: 'string',
+                short: 'a'
+            },
+            'help': {
+                type: 'boolean',
+                short: 'h'
+            }
+        },
+        allowPositionals: true
+    });
+
+    // there's probably a prettier way as to not have to reassign this just to
+    // make tsc happy, but I'm a little exhausted...
+    const args: string[] = positionals as string[];
+
+    if (values.help != undefined) {
+        const exec = [
+            'ts-node',
+            path.join(path.basename(__dirname), path.basename(__filename))
+        ].join(' ');
+        console.log(usage(exec));
+        process.exit(1);
+    }
+
+    values = {...defaultOptargs, ...values};
+
+    var errors: boolean = false;
+
+    for (var requiredOptarg of requiredOptargs) {
+        if (!(requiredOptarg in values)) {
+            console.error(
+                `error: missing options argument: --${requiredOptarg}`
             );
-            return true;
+            errors = true;
         }
     }
-);
 
-[
-    'LICENSE',
-    'README.md',
-].forEach((target) => {
-    // TODO: sync file stats
-    fs.cpSync(
-        path.join(cwd, target),
-        path.join(tempDir, target),
-        {
-            recursive: true,
-            filter: (src: string, dest: string) => {
-                console.log(
-                    `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
-                );
-                return true;
-            }
+    if (args.length < minPosargs) {
+        if (args.length == 1) {
+            args.push(DEFAULT_OUTPUT_DIR);
         }
-    );
-});
 
-if (argv.docsDir) {
-    console.log('docs supplied, will copy...')
-    // TODO: sync file stats
-    fs.cpSync(
-        path.join(cwd, argv.docsDir),
-        path.join(tempDir, `_docs`),
-        {
-            recursive: true,
-            filter: (src: string, dest: string) => {
-                console.log(
-                    `cp: ${path.relative(cwd, src)} > ${path.relative(cwd, dest)}`
-                );
-                return true;
-            }
+        else if (args.length == 0) {
+            args.push(DEFAULT_INPUT_DIR);
+            args.push(DEFAULT_OUTPUT_DIR);
         }
-    );
-}
-
-console.log('generating index (\'assets.txt\') of assets...')
-// TODO: sync file stats
-var fd: number = fs.openSync(path.join(tempDir, 'assets.txt'), 'w');
-// NOTICE: MUST use POSIX path separators
-// iterating over globbing search results, to avoid memory pressure
-for (const match of globIterateSync(
-    [argv.inputDir, '**'].join('/'),
-    { withFileTypes: true }
-)) {
-    if (match.isDirectory()) continue;
-    const relPath = path.relative(argv.inputDir, match.relative());
-    fs.writeSync(fd, `${relPath}\n`);
-}
-fs.closeSync(fd);
-
-console.log('generating npm package specification \'package.json\'...');
-const packageSpec = JSON.parse(fs.readFileSync(
-    'package.json',
-    { encoding: 'utf-8' }
-));
-fs.writeFileSync(
-    path.join(tempDir, 'package.json'),
-    JSON.stringify({
-        'name': packageSpec.name,
-        'version': packageSpec.version,
-        'description': packageSpec.description,
-        'author': packageSpec.author,
-        'private': true
-    }, null, 4)
-);
-
-var outputDir = path.resolve(path.join(cwd, argv.outputDir));
-console.log(`mkdir: ${outputDir}`);
-fs.mkdirSync(outputDir, { recursive: true });
-
-console.log(`npm: pack --pack-destination ${outputDir}`);
-child_process.execSync(
-    `npm pack --pack-destination ${outputDir}`,
-    {
-        cwd: tempDir,
-        stdio: "inherit"
     }
-);
+
+    if (errors != false) {
+        console.log(`supply -h/--help, for more information.`)
+        process.exit(1);
+    }
+
+    pack({
+        inputDir: args[0],
+        outputDir: args[1],
+        docsInputDir: args[2] ?? null,
+        assetsIndexBasename: values['assets-index-basename']!,
+        docsOutputDirname: values['docs-dirname']!,
+    });
+}
