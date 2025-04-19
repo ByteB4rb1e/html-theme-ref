01881146d0c583514b5f71c29bafa297817e45c3
Rodney, Tiara <tiara.rodney@administratrix.de>
2025-04-19 22:35:22 +0200

    refactor(webpack): change docs workflow

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -1,97 +1,281 @@
-const chokidar = require('chokidar'); // depdendency of webpack
-const escapeHtml = require('escape-html');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const fs = require('fs');
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const escapeHtml = require('escape-html');
+
 const config = require('./webpack.config.debug.js');
 
+
 /**
- * recursively search files by suffix
  *
- * @param root - path to root directory
- * @param suffix - file suffix to filter for
  */
-function findFilesBySuffix(root, suffix) {
-    if (!fs.existsSync(root)) {
-        throw new Error(`path does not exist: '${startPath}'`, startPath);
+class StyleDocumentationPlugin {
+    static defaultOptions = {
+        inputBasedir: path.join('docs', 'style'),
+        outputBasedir: path.join('docs', 'style'),
+        frameTemplatePath: path.join('docs', 'style', '_templates', 'frame.html'),
+        indexTemplatePath: path.join('docs', 'style', '_templates', 'index.html'),
     }
 
-    var out = [];
-    var files = fs.readdirSync(root);
+    /**
+     *
+     */
+    constructor(options = {}) {
+        const pluginName = StyleDocumentationPlugin.name;
 
-    for (var i = 0; i < files.length; i++) {
-        let filename = path.join(root, files[i]);
-        let stat = fs.lstatSync(filename);
+        this.options = {
+            ...StyleDocumentationPlugin.defaultOptions,
+            ...options
+        };
 
-        if (stat.isDirectory()) {
-            out = [...out, ...findFilesBySuffix(filename, suffix)];
+        this.framesOptions = [];
+        this.indexOptions = null;
+        this.logger = null;
+
+        this.onBeforeRun = this.onBeforeRun.bind(this);
+    }
+
+    /**
+     *
+     */
+    apply(compiler) {
+        const pluginName = StyleDocumentationPlugin.name;
+
+        this.logger = compiler.getInfrastructureLogger(pluginName);
+
+        compiler.hooks.beforeRun.tap(pluginName, this.onBeforeRun);
+        compiler.hooks.watchRun.tap(pluginName, this.onBeforeRun);
+
+        for (const frameOptions of StyleDocumentationPlugin.getFrames(
+            this.options.inputBasedir,
+            this.options.outputBasedir,
+            this.options.frameTemplatePath
+        )) {
+            this.framesOptions.push(frameOptions);
+            this.logger.info(
+                'cached frame options for:',
+                frameOptions.templateParameters.sourcePath
+            );
         }
-        else if (filename.endsWith(suffix)) { out.push(filename) }
-    };
 
-    return out;
-};
+        const indexFilename = path.join(this.options.outputBasedir, 'index.html');
 
-const BUILDTARGET = 'doc';
-const BUILDDIR = 'build';
-const DOC_STYLE_EXT = '.htm';
-const DOC_STYLE_PATH = ['docs', 'style'];
-const DOC_SANDBOX_BASEDIR = '';
-const DOC_STYLE_PARTIALS = findFilesBySuffix(path.join(...DOC_STYLE_PATH), DOC_STYLE_EXT);
+        this.indexOptions = {
+            title: "Tiara's HTML Theming Reference (Usability Demonstration)",
+            filename: indexFilename,
+            template: this.options.indexTemplatePath,
+            // don't inject styles, we don't want the index to look pretty as
+            // this could be make the frames be difficult to discern
+            inject: false,
+            templateParameters: {
+                // description is going to be rendered as HTML so the extra
+                // spaces are going to be dropped anyway.
+                description: `This usability demonstration serves both as a a
+                              showcase, as well as a usability test. It mirrors
+                              the Sass 7:1 pattern of the style's source code,
+                              in order to have a structured approach for
+                              maintaing coverage. To use hot-reloading during
+                              development, open each iframe in a seperate
+                              window.`,
+                framesMeta: StyleDocumentationPlugin.getGroupedFramesMeta(
+                    this.framesOptions,
+                    indexFilename,
+                )
+            }
+        }
 
-config.output.path = path.resolve(__dirname, path.join(BUILDDIR, BUILDTARGET));
-
-var out = {};
-
-for (var i = 0; i < DOC_STYLE_PARTIALS.length; i += 1) {
-
-    let dirname = DOC_STYLE_PARTIALS[i].split(path.sep);
-    let basename = dirname.splice(dirname.length - 1, 1);
-
-    let removed = dirname.splice(0, DOC_STYLE_PATH.length);
-    if (path.join(...removed) !== path.join(...DOC_STYLE_PATH)) {
-        throw new Error(`${removed} != ${DOC_STYLE_PATH}`);
+        this.logger.info(
+            'cached frame options for:', 
+            this.options.indexTemplatePath
+        );
     }
 
-    let filename = path.join(
-        DOC_SANDBOX_BASEDIR,
-        path.join(...dirname),
-        `${path.basename(path.join(...basename), DOC_STYLE_EXT)}.html`
-    );
+    /**
+     *
+     */
+    onBeforeRun(compiler) {
+        new HtmlWebpackPlugin(this.indexOptions).apply(compiler);
 
-    let c = {
-        title: `Usability test for ${filename}`,
-        filename: filename,
-        inject: true,
-        isDevServer: process.env.WEBPACK_SERVE,
-        template: 'docs/style/_templates/sandbox.html',
-        templateParameters: {
-            partial: fs.readFileSync(DOC_STYLE_PARTIALS[i])
-        },
-    };
+        // TODO: refactor to run asynchronously
+        this.framesOptions.forEach(frameOptions => {
+            new HtmlWebpackPlugin(frameOptions).apply(compiler);
+        })
 
-    let outDir = path.dirname(filename);
+        compiler.hooks.initialize.call(compiler);
+    }
 
-    if (out[outDir] === undefined) { out[outDir] = [] }
+    /**
+     * read a file from the local filesystem and escape special HTML characters,
+     * if required
+     *
+     * @param filePath string - path to a local file
+     * @param escapeHtmlChars boolean - whether to escape HTML special character
+     *                                  or not
+     */
+    static readFile(filePath, escapeHtmlChars = false) {
+        const data = fs.readFileSync(filePath);
 
-    let raw = fs.readFileSync(DOC_STYLE_PARTIALS[i]);
+        if (escapeHtmlChars) return escapeHtml(data);
 
-    out[outDir].push([filename, escapeHtml(raw)]);
+        return data;
+    }
 
-    config.plugins.push(new HtmlWebpackPlugin(c));
+    /**
+     * recursively search files by suffix
+     *
+     * @param root string - path to root directory
+     * @param suffix string - file suffix to filter for
+     */
+    static *findFilesBySuffix(root, suffix) {
+        if (!fs.existsSync(root)) {
+            throw new Error(`path does not exist: '${root}'`);
+        }
+
+        var out = [];
+        var files = fs.readdirSync(root);
+
+        for (var i = 0; i < files.length; i++) {
+            let filename = path.join(root, files[i]);
+            let stat = fs.lstatSync(filename);
+
+            if (stat.isDirectory()) {
+                yield* StyleDocumentationPlugin.findFilesBySuffix(filename, suffix)
+            }
+            else if (filename.endsWith(suffix)) { yield filename }
+        };
+
+        return out;
+    }
+
+    /**
+     * get basic options of style documentation frame for building through the
+     * html-webpack-plugin
+     *
+     * @param inputFilePath string - path to source file (partial HTML document)
+     * @param inputBaseDir string  - base directory of all partials
+     * @param outputBaseDir string - base directory to output partials to
+     *
+     * @returns partial options object for HtmlWebpackPlugin
+     */
+    static getBasicFrameOptions(
+        inputFilePath,
+        inputBaseDir,
+        outputBaseDir,
+    ) {
+        // canonical name of frame
+        const outputName = path.basename(
+            inputFilePath,
+            path.extname(inputFilePath)
+        );
+
+        // canonical directory name of frame, applicable to both input and output
+        const relDirname = path.dirname(
+            path.relative(inputBaseDir, inputFilePath)
+        );
+
+        return {
+            title: [
+                'Usability test for',
+                `${relDirname.replace('\\', '/')}/${outputName}`,
+            ].join(' '),
+            filename: path.join(
+                outputBaseDir,
+                relDirname,
+                `${outputName}.html`
+            ),
+            templateParameters: {
+                relDirname: relDirname.replace('\\', '/'),
+                sourcePath: inputFilePath,
+            }
+        };
+    }
+
+    /**
+     * get full configuration options of all style documentation frames for building
+     * through the html-webpack-plugin
+     *
+     * @param inputBaseDir string  - base directory of all frame partials
+     * @param outputBaseDir string - base directory to output frame partials to
+     * @param templatePath string - template to expand frame partials with
+     *
+     * @returns generator of configuration options for html-webpack-plugin
+     *          constructor
+     */
+    static *getFrames(
+        inputBasedir,
+        outputBasedir,
+        templatePath,
+    ) {
+        for (const filePath of StyleDocumentationPlugin.findFilesBySuffix(inputBasedir, '.htm')) {
+            const basicOptions = StyleDocumentationPlugin.getBasicFrameOptions(
+                filePath,
+                inputBasedir,
+                outputBasedir,
+            );
+
+            yield {
+                ...basicOptions,
+                ...{
+                    inject: true,
+                    isDevServer: process.env.WEBPACK_SERVE,
+                    template: templatePath,
+                    templateParameters: {
+                        ...basicOptions.templateParameters,
+                        ...{
+                            getContent: StyleDocumentationPlugin.readFile,
+                        }
+                    },
+                }
+            };
+        }
+    }
+
+    /**
+     * get metadata of a frame required by index
+     */
+    static getFrameMeta(
+        frameOptions,
+        indexFilename,
+    ) {
+        return {
+            href: path.relative(
+                path.dirname(indexFilename),
+                frameOptions.filename
+            ).replace('\\', '/'),
+            sourcePath: frameOptions.templateParameters.sourcePath,
+            getContent: (path) => StyleDocumentationPlugin.readFile(path, true)
+        }
+    }
+
+    static getGroupedFramesMeta(framesOptions, indexFilename) {
+        const groups = {};
+
+        for (const frame of framesOptions) {
+            const relDirname = frame.templateParameters.relDirname;
+
+            if (!(relDirname in groups)) groups[relDirname] = [];
+
+            groups[relDirname].push(
+                StyleDocumentationPlugin.getFrameMeta(frame, indexFilename)
+            );
+        }
+
+        return groups;
+    }
 }
 
-config.plugins.push(new HtmlWebpackPlugin({
-    title: "Tiara's HTML Theming Reference (Usability Demonstration)",
-    template: "docs/style/_templates/index.html",
-    inject: false,
-    templateParameters: {
-        sandboxes: out,
-        description: "This usability demonstration serves both as a a showcase, as well as a usability test. It mirrors the Sass 7:1 pattern of the style's source code, in order to have a structured approach for maintaing coverage. To use hot-reloading during development, open each iframe in a seperate window."
-    }
-}));
+config.output.path = path.resolve(__dirname, 'build', 'doc');
+
+// TODO: search for ts-loader instances, instead of hard-coding...
+config.module.rules[1].use[0].options.configFile = 'tsconfig.doc.json';
+// ensure that generated maps are emitted to the correct directory, in case
+// something different is specified in the tsconfig and I forgot about it
+config.module.rules[1].use[0].options.compilerOptions = {
+    "outDir": path.join(config.output.path, 'script')
+};
+
+config.plugins.push(new StyleDocumentationPlugin());
 
 module.exports = {
     ...config,
@@ -102,36 +286,6 @@ module.exports = {
         ],
     },
     devServer: {
-        setupMiddlewares: (middlewares, devServer) => {
-            if (!devServer) {
-                throw new Error('webpack-dev-server is not defined');
-            }
-
-            // Watch .htm files manually, since they're not part of the
-            // dependency graph (and shouldn't be)
-            chokidar.watch(
-                findFilesBySuffix('docs', '.htm'),
-                // TODO: Improve environment detection for MinGW and Cygwin
-                // polling is pretty inefficient...
-                {
-                    usePolling: true,
-                }
-            ).on('change', (filePath) => {
-                console.log(`${filePath} changed. Reloading...`);
-                // documented absolutely nowhere...
-                // luckily (almost) opened a PR for something else, so I still
-                // knew the basic layout of the repository. 'content-changed'
-                // does not work, though 'static-changed' does...
-                //
-                // https://github.com/webpack/webpack-dev-server/blob/fb38571a90c54f01b79fccd5c2b7e5113abef315/lib/Server.js#L3378
-                devServer.sendMessage(
-                    devServer.webSocketServer.clients,
-                    "static-changed"
-                );
-            });
-
-            return middlewares;
-        },
         compress: true,
         open: true,
         hot: true,
