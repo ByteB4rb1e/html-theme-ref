43d4be74be8ae5a58d71ad3502043875bd11664f
Rodney, Tiara <tiara.rodney@administratrix.de>
2025-04-20 04:40:24 +0200

    refactor(webpack): add explicit tsconfig outDir for debug target

--- webpack.config.debug.js
+++ webpack.config.debug.js
@@ -2,7 +2,11 @@ const path = require("path");
 const config = require('./webpack.config.js');
 
 config.output.path = path.resolve(__dirname, 'build/debug');
+// TODO: search for ts-loader instances, instead of hard-coding...
 config.module.rules[1].use[0].options.configFile = 'tsconfig.debug.json';
+config.module.rules[1].use[0].options.compilerOptions = {
+    "outDir": path.join(config.output.path, 'script')
+};
 
 // disable CSS minification
 //config.module.rules[0].use.splice(0, 1);
