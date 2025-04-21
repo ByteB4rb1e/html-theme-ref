d3c820ae1d4b0a56034089f9aaebf5f26561f9a2
Rodney, Tiara <tiara.rodney@administratrix.de>
2025-04-21 20:35:41 +0200

    chore: init config for Oddbird True Scss testing framework

---
+++ jest.config.sass-true.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigurationWithTsJest} **/
+module.exports = {
+    testEnvironment: "node",
+    transform: {
+        "^.+.tsx?$": [
+            "ts-jest",
+            {
+                tsconfig: 'tsconfig.debug.json'
+            }
+        ]
+    },
+    roots: ['./tests/script']
+}
